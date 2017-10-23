3513d07cec2179cc9b9ba7bc8c3b80611eb6a9c5
Mason <htj889@gmail.com>
2017-10-23 10:56:54 +0900

    Apply the MVC pattern.

--- app/src/main/java/com/mason/kakao/masonsgallary/view/ImagesActivity.java
+++ app/src/main/java/com/mason/kakao/masonsgallary/view/ImagesActivity.java
@@ -1,12 +1,17 @@
 package com.mason.kakao.masonsgallary.view;
 
 import android.Manifest;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -17,11 +22,10 @@ import com.mason.kakao.masonsgallary.MasonApplication;
 import com.mason.kakao.masonsgallary.R;
 import com.mason.kakao.masonsgallary.base.BaseActivity;
 import com.mason.kakao.masonsgallary.base.SimpleObserver;
+import com.mason.kakao.masonsgallary.model.ImagesRepository;
 import com.mason.kakao.masonsgallary.model.data.ImageData;
 import com.mason.kakao.masonsgallary.model.data.Tag;
 import com.mason.kakao.masonsgallary.view.adapter.ImagesAdapter;
-
-import org.reactivestreams.Subscription;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,33 +34,127 @@ import io.reactivex.Observable;
 import io.reactivex.annotations.NonNull;
 
 public class ImagesActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private RecyclerView recyclerView;
-    private ImagesAdapter imagesAdapter;
-    private ProgressBar progressBar;
-    private NavigationView navigationView;
-    private DrawerLayout drawerLayout;
+    private RecyclerView mRecyclerView;
+    private ImagesAdapter mImagesAdapter;
+    private ProgressBar mProgressBar;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestPermission();
+        checkPermissions();
     }
 
     @Override
     public void setupActivity() {
         setContentView(R.layout.activity_images);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        imagesAdapter = new ImagesAdapter(this);
-        recyclerView.setAdapter(imagesAdapter);
-        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        navigationView = (NavigationView) findViewById(R.id.navigation);
-        navigationView.setNavigationItemSelectedListener(this);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mImagesAdapter = new ImagesAdapter(this);
+        mRecyclerView.setAdapter(mImagesAdapter);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        setupDrawer();
     }
 
-    private void requestPermission() {
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@android.support.annotation.NonNull MenuItem item) {
+        Tag tag = null;
+        switch(item.getItemId()) {
+            case R.id.menu_ryan:
+                tag = Tag.Ryan;
+                break;
+            case R.id.menu_muzi:
+                tag = Tag.Muzi;
+                break;
+            case R.id.menu_apeach:
+                tag = Tag.Apeach;
+                break;
+            case R.id.menu_frodo:
+                tag = Tag.Frodo;
+                break;
+            case R.id.menu_neo:
+                tag = Tag.Neo;
+                break;
+            case R.id.menu_jay_g:
+                tag = Tag.Jay_G;
+                break;
+            case R.id.menu_con:
+                tag = Tag.Con;
+                break;
+        }
+
+        if(tag != null) {
+            Observable<List<ImageData>> observable = MasonApplication.get(this).getImagesRepository().getTaggedList(tag);
+            setLoadingIndicator(true);
+            observable.subscribe(new SimpleObserver<List<ImageData>>() {
+                @Override
+                public void onNext(@NonNull List<ImageData> list) {
+                    setLoadingIndicator(false);
+                    mImagesAdapter.setList(list);
+                }
+
+                @Override
+                public void onError(@NonNull Throwable e) {
+                    setLoadingIndicator(false);
+                }
+            });
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    private void setupDrawer() {
+        mNavigationView = (NavigationView) findViewById(R.id.navigation);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    private void checkPermissions() {
         TedPermission.with(this)
                 .setPermissionListener(new PermissionListener() {
                     @Override
@@ -80,7 +178,7 @@ public class ImagesActivity extends BaseActivity implements NavigationView.OnNav
                     @Override
                     public void onNext(@NonNull List<ImageData> list) {
                         setLoadingIndicator(false);
-                        imagesAdapter.setList(list);
+                        mImagesAdapter.setList(list);
                     }
 
                     @Override
@@ -91,55 +189,7 @@ public class ImagesActivity extends BaseActivity implements NavigationView.OnNav
     }
 
     private void setLoadingIndicator(boolean load) {
-        recyclerView.setVisibility(load ? View.GONE : View.VISIBLE);
-        progressBar.setVisibility(load ? View.VISIBLE : View.GONE);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@android.support.annotation.NonNull MenuItem item) {
-        Observable<List<ImageData>> observable = null;
-        switch(item.getItemId()) {
-            case R.id.menu_ryan:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Ryan);
-                break;
-            case R.id.menu_muzi:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Muzi);
-                break;
-            case R.id.menu_apeach:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Apeach);
-                break;
-            case R.id.menu_frodo:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Frodo);
-                break;
-            case R.id.menu_neo:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Neo);
-                break;
-            case R.id.menu_jay_g:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Jay_G);
-                break;
-            case R.id.menu_con:
-                observable = MasonApplication.get(this).getImagesRepository().getTaggedList(Tag.Con);
-                break;
-        }
-        if(observable == null) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return false;
-        }
-
-        setLoadingIndicator(true);
-        observable.subscribe(new SimpleObserver<List<ImageData>>() {
-                     @Override
-                     public void onNext(@NonNull List<ImageData> list) {
-                         setLoadingIndicator(false);
-                         imagesAdapter.setList(list);
-                     }
-
-                     @Override
-                     public void onError(@NonNull Throwable e) {
-                         setLoadingIndicator(false);
-                     }
-                 });
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return false;
+        mRecyclerView.setVisibility(load ? View.GONE : View.VISIBLE);
+        mProgressBar.setVisibility(load ? View.VISIBLE : View.GONE);
     }
 }
